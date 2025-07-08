6288de615f9953ff267e02cea45e2a0cd891b6f1
SnazzyNivesh522 <niveshpritmani@gmail.com>
2025-07-08 11:21:50 +0000

    automated commands through scripts

---
+++ script.sh
@@ -0,0 +1,11 @@
+source .venv/bin/activate
+
+python3 update_capec_db.py 
+python3 update_cwe_db.py 
+python3 update_technique_db.py 
+python3 retrieve_cve.py 
+python3 cve2cwe.py 
+python3 cwe2capec.py 
+python3 capec2technique.py
+
+deactivate
