5c094a6d7396815db84230f1b3a9b4d77fcc4f0b
SnazzyNivesh522 <niveshpritmani@gmail.com>
2025-07-09 05:54:34 +0000

    update conn string

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 def get_session():
 
     try:
-        return AsyncIOMotorClient("mongodb://nvd:nvd@172.20.0.2:27017/")
+        return AsyncIOMotorClient("mongodb://dev:dev@172.20.0.2:27017/")
 
     except Exception as e:
         raise Exception(e)
