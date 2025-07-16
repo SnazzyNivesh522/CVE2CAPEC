1cfba640ff737fe821d0ebe6f21276987c454d94
SnazzyNivesh522 <niveshpritmani@gmail.com>
2025-07-16 10:09:19 +0000

    update data cve cwe capec attack

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 source .venv/bin/activate
 
-python3 update_capec_db.py 
-python3 update_cwe_db.py 
-python3 update_technique_db.py 
-python3 retrieve_cve.py 
-python3 cve2cwe.py 
-python3 cwe2capec.py 
+python3 update_capec_db.py
+python3 update_cwe_db.py
+python3 update_technique_db.py
+python3 retrieve_cve.py
+python3 cve2cwe.py
+python3 cwe2capec.py
 python3 capec2technique.py
+
+python3 update_database.py
 
 deactivate
